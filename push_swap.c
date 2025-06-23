2c912bcd59085cd16fb49210206076dae29795d5
root <root@DESKTOP-S9CKJUO.localdomain>
2025-06-23 10:52:57 +0300

    first commit

--- push_swap.c
+++ push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jissa <jissa@student.42.fr>                +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 17:37:39 by jissa             #+#    #+#             */
-/*   Updated: 2025/06/21 18:38:28 by jissa            ###   ########.fr       */
+/*   Updated: 2025/06/23 08:07:12 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,18 @@ typedef struct lin_list {
 	int				data;
 } Node;
 
+int	ft_isdigit(char *str)
+{
+	int i = 0;
+	while(str[i])
+	{
+		if(str[i] >= '0' && str[i] <= '9')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 Node	*append_list(int num)
 {
 	Node *list = malloc(sizeof(Node));
@@ -28,9 +40,28 @@ Node	*append_list(int num)
 	return list;
 }
 
+char		*check_dup(Node **Node_list)
+{
+	Node *checker_outer = *Node_list;
+	Node *checker_inner;
+	while(checker_outer != NULL)
+	{
+		checker_inner = checker_outer->next;
+		while(checker_inner != NULL)
+		{
+			if (checker_outer->data == checker_inner->data)
+				return ("duplicates found");
+			checker_inner = checker_inner->next;
+		}
+		checker_outer = checker_outer->next;
+	}
+	return ("no duplicates found");
+}
+
 int main(int argc, char **argv)
 {
 	int i = 0;
+	int j = 0;
 	Node *Node_head = NULL;
 	Node *new_Node = NULL;
 	
@@ -40,15 +71,27 @@ int main(int argc, char **argv)
 	if (argc == 2)
 	{
 		char **res = ft_split(argv[1], ' ');
+		while(res[j])
+		{
+			printf("%d\n", ft_isdigit(res[j]));
+			j++;
+		}
 		number = ft_atoi(res[i]);
 		Node_head = append_list(number);
+		new_Node = Node_head;
 		i += 1;
 		while(res[i])
 		{
 			number = (ft_atoi(res[i]));
-			new_Node = append_list(number);
-			new = list->next;
+			new_Node->next = append_list(number);
+			new_Node = new_Node->next;
 			i++;
+		}
+		printf("%s\n", check_dup(&Node_head));
+		while(Node_head != NULL)
+		{
+			printf("%d\n", Node_head->data);
+			Node_head = Node_head->next;
 		}
 	}
 }
